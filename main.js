bb2c2c7a3fca2b44250e4850becd9b7f1edb8e29
jamesjo <jamescakr@gmail.com>
2024-07-04 22:23:03 -0600

    last commit

--- main.js
+++ main.js
@@ -3,11 +3,32 @@ let addButton = document.getElementById("add-button");
 let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
 let underLine = document.getElementById("under-line");
+let mode = "all";
+let filterList = [];
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
 addButton.addEventListener("click", addTask);
+
+taskInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
 taskInput.addEventListener("focus", () => (taskInput.value = ""));
 
 function addTask() {
+  let taskContent = taskInput.value.trim();
+
+  if (taskContent === "") {
+    return;
+  }
+
   let task = {
     id: randomIDGenerate(),
     taskContent: taskInput.value,
@@ -16,36 +37,41 @@ function addTask() {
   taskList.push(task);
   console.log(taskList);
   render();
+
+  taskInput.value = "";
 }
 
 function render() {
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete) {
       resultHTML += `<div class="task task-done-bg">
-                    <div class="task-done">${taskList[i].taskContent}</div>
+                    <div class="task-done">${list[i].taskContent}</div>
                     <div>
-                        <button onclick="toggleComplete('${taskList[i].id}')">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHjlPHXXPWAGMtZWtEC4Rn6GONLAJSg3uEDQ&s */
-" alt="Undo" style="width: 20px; height: 20px;" /> 
+                        <button onclick="toggleComplete('${list[i].id}')">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHjlPHXXPWAGMtZWtEC4Rn6GONLAJSg3uEDQ&s" alt="Undo" style="width: 20px; height: 20px;" /> 
                         </button>
-                        <button onclick="deleteTask('${taskList[i].id}')">
-                        <img src="https://cdn-icons-png.flaticon.com/512/6861/6861362.png
-" alt="Delete" style="width: 20px; height: 20px;" />
+                        <button onclick="deleteTask('${list[i].id}')">
+                        <img src="https://cdn-icons-png.flaticon.com/512/6861/6861362.png" alt="Delete" style="width: 20px; height: 20px;" />
                         </button>
                     </div>
                 </div>`;
     } else {
       resultHTML += `<div class="task">
-      <div>${taskList[i].taskContent}</div>
+      <div>${list[i].taskContent}</div>
       <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">
-          <img src="https://www.iconpacks.net/icons/2/free-check-icon-3278-thumb.png
-" alt="Check" style="width: 20px; height: 20px;" /> 
+          <button onclick="toggleComplete('${list[i].id}')">
+          <img src="https://www.iconpacks.net/icons/2/free-check-icon-3278-thumb.png" alt="Check" style="width: 20px; height: 20px;" /> 
           </button>
-          <button onclick="deleteTask('${taskList[i].id}')">
-          <img src="https://cdn-icons-png.flaticon.com/512/6861/6861362.png
-" alt="Delete" style="width: 20px; height: 20px;" /> 
+          <button onclick="deleteTask('${list[i].id}')">
+          <img src="https://cdn-icons-png.flaticon.com/512/6861/6861362.png" alt="Delete" style="width: 20px; height: 20px;" /> 
           </button>
       </div>
   </div>`;
@@ -62,7 +88,49 @@ function toggleComplete(id) {
       break;
     }
   }
+  filterList = [];
+  if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (!taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
+    }
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
+    }
+  }
   render();
+}
+
+function filter(event) {
+  console.log("filter", event.target.id);
+  mode = event.target.id;
+
+  underLine.style.left = event.currentTarget.offsetLeft + "px";
+  underLine.style.width = event.currentTarget.offsetWidth + "px";
+
+  filterList = [];
+  if (mode === "all") {
+    render();
+  } else if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+    console.log("진행중", filterList);
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
 }
 
 function deleteTask(id) {
@@ -70,6 +138,20 @@ function deleteTask(id) {
     if (taskList[i].id == id) {
       taskList.splice(i, 1);
       break;
+    }
+  }
+  filterList = [];
+  if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (!taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
+    }
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
     }
   }
   render();
